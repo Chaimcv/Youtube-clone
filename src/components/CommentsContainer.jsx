835ba902ca297e0f8aa4253d28587e6c19b7ab59
Chaimcv <chaithanyamcv@gmail.com>
2026-02-05 14:14:47 +0530

    updates

--- src/components/CommentsContainer.jsx
+++ src/components/CommentsContainer.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const CommentsContainer = () => {
-  return (
-    <div className='m-2 p-2'>
-        <h1 className='font-bold'>Comments</h1>
-    </div>
-  )
+  const commentsData = [             //Dummy comment-nesting  section implementation
+    {
+      name: "Akshay",
+      text: "Comments...",
+      replies: [],
+    },
+    {
+      name: "Akshay 2",
+      text: "Comments nesting",
+      replies: [
+        {
+          name: "Akshay",
+          text: "Comments...nested",
+          replies: [
+            {
+              name: "Akshay",
+              text: "Comments...",
+              replies: [],
+            },
+          ],
+        },
+      ],
+    },
+     {
+        name:"Akshay-last",
+        text:"Comments...",
+        replies:[]
 }
+  ];
 
-export default CommentsContainer
+  return (
+    <div className="m-2 p-2">
+      <h1 className="font-bold">Comments</h1>
+    </div>
+  );
+};
+
+export default CommentsContainer;
