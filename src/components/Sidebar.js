8c46657fc65969ff2a2063725b586cb782576b3d
Chaimcv <chaithanyamcv@gmail.com>
2026-02-05 18:42:54 +0530

    nested comments

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,20 +9,20 @@ const Sidebar = () => {
     <div className='p-5 shadow-md w-50'>
         
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li>Shorts</li>
-            <li>Subscriptions</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'><Link to="/"><img className="w-10 h-10 inline-flex" src="https://endlessicons.com/wp-content/uploads/2012/11/home-icon-614x460.png" alt='Home' />Home</Link></li>
+            <li className=' hover:bg-slate-100 rounded-lg'> <img className="w-5 h-5 inline-flex m-2" src="https://img.icons8.com/?size=24&id=ajczeHCWXbyR&format=png" alt='shorts' />  Shorts</li>
+            <li className=' hover:bg-slate-100 rounded-lg'><img className="w-5 h-5 inline-flex m-2" src="https://img.icons8.com/?size=100&id=DA3vWy7pLcg6&format=png&color=000000" alt="subscription" />Subscriptions </li>
         </ul>
          <h1 className='font-bold'>You</h1>
         <ul>
-            <li>History</li>
-            <li>Playlist</li>
-            <li>Watch Later</li>
-            <li>Liked Videos</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'><img className="w-5 h-5 m-2 inline-flex" src="https://seekicon.com/free-icon-download/history_8.svg" />History</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'><img className="w-10 h-10 inline-flex" />Playlist</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'><img className="w-10 h-10 inline-flex" />Watch Later</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'><img className="w-10 h-10 inline-flex" />Liked Videos</li>
         </ul>
         <h1 className='font-bold'>Explore</h1>
         <ul>
-            <li>Shopping</li>
+            <li  className=' hover:bg-slate-100 rounded-lg'>Shopping</li>
             <li>Music</li>
             <li>Films</li>
             <li>Show more</li>
