3daa8bb1eede140c786d0c5653e04a3c84bfd425
Chaimcv <chaithanyamcv@gmail.com>
2026-02-09 20:02:30 +0530

    updates

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ChatMessages = ({name,messsage}) => {
   return (
-    <div className='flex inline-block'>
+    <div className='flex inline-block p-1'>
        <img
-          className="h-5"
+          className="h-5 pl-1"
           src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
           alt="user-icon"
         ></img>
-     <span className='font-medium'>{name} </span>
+     <span className='font-medium pl-1'>{name} </span>
       <span> -{messsage}</span>
     </div>
   )
