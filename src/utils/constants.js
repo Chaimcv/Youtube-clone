ee26547222e402f999614b564897c45fa85730e1
Chaimcv <chaithanyamcv@gmail.com>
2026-02-10 11:32:10 +0530

    updates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,3 +11,5 @@ export const LIVE_CHAT_COUNT=20;
 
 //const GOOGLE_API_KEY="AIzaSyDbAStQVAX0EZTHgxJgNeHlYr2jZIKarBU"
 //export const YOUTUBE_VIDEOS_API=  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key='+GOOGLE_API_KEY;
+
+const YOUTUBE_VIDEO_BYID="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=Ks-_Mh1QhMc&key=";
